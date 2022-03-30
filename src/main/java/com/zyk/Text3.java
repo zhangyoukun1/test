763f25a07d7d963b83bb57a475d4d875d0b7b781
zhangyoukun1 <41310653+zhangyoukun1@users.noreply.github.com>
2022-03-30 15:46:13 +0800

    hello

--- src/main/java/com/zyk/Text3.java
+++ src/main/java/com/zyk/Text3.java
@@ -9,13 +9,14 @@ public class Text3 {
         try {
 
 
-            //Runtime.getRuntime().exec("git init");
+            Runtime.getRuntime().exec("git init");
             Runtime.getRuntime().exec("git remote add origin git@github.com:zhangyoukun1/test.git");
 
             Runtime.getRuntime().exec("git add .");
             Runtime.getRuntime().exec("git commit -m \"hello\"");
             Runtime.getRuntime().exec("git pull --rebase origin master");
             Runtime.getRuntime().exec("git push -u origin master");
+            System.out.println("执行完成");
         } catch (IOException e) {
             e.printStackTrace();
         }
